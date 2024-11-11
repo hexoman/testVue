993234dd99ddfb869c8bc54ca16c9f1a75a7a9a7
hexoman <dyrenshizaisheng@163.com>
2024-11-11 22:35:00 +0800

    dd

---
+++ assets/index.BeurTFNn.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./index-ocBV1Jcm.js";function e(e,n={}){const s=Object.keys(n).map((o=>`${o}=${encodeURIComponent(n[o])}`)).join("&"),c=s?`${e}?${s}`:e;console.log("fullUrl",c),o({url:c,success:function(o){console.log("Navigate to page successfully",o)},fail:function(o){console.error("Failed to navigate to page",o)}})}export{e as n};
